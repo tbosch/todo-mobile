a7a84dd8f0db5396ab8bf0a3709668966ac19972
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2011-10-18 17:02:36 +0200

    now all files are published to gh-pages

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -6,8 +6,6 @@ if $M2_HOME/bin/mvn package; then
     mkdir -p $TMP
     rm -fr $TMP
     cp -r $SRC/ $TMP
-	find $TMP -name "*.js" ! -name "main*.js" ! -name "require.js" -exec rm "{}" ";"    
-    find $TMP -depth -type d -empty -exec rmdir "{}" ";"
     git stash
     if git checkout gh-pages; then
       rm -fr *
